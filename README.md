6a3351cf88609b2ac898df926f2d268f71c81f91
Achyuth <achyuthc@gmail.com>
2023-10-22 14:21:23 -0400

    Save first iteration of trained xgboost model

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # UFC-Fight-Predictor
+
 This is a model based on UFC data which uses fighter statistics to determine who will win a future fight.
+
+## Install
+
+pip install
+
+## Guide
+
+- To get seperated training data from the current UFC data set:
+  **from ufc_data import X_train, X_test, y_train, y_test**
+
+- Train and save new models from ufc_model.py
+
+- Hypertune parameters in optune_tuning.py
+
+- Trained models are stored in ./models
--- ufc_model.py
+++ ufc_model.py
@@ -5,13 +5,19 @@ from sklearn.ensemble import RandomForestClassifier
 from ufc_data import X_train, X_test, y_train, y_test
 
 #Gradient Boost Model using hyper parameter obtained through Optuna trial tuning
-gradientBoostModel = xgb.XGBClassifier(reg_lambda=0.02346830703895387,alpha=9.956365178312454, tree_method='gpu_hist', objective='binary:logistic', verbosity=0,n_jobs=-1,learning_rate=0.030042259864304366,min_child_weight=19,max_depth=12,max_delta_step=8,subsample=0.21684497186514273, colsample_bytree=0.7003826597151147, gamma=0.02156806207076522, n_estimators=147, eta=0.15337337361538475)
+gradientBoostParams = {'lambda': 0.02346830703895387, 'alpha': 9.956365178312454, 'tree_method': 'gpu_hist', 'objective': 'binary:logistic', 'verbosity': 0, 'n_jobs': -1, 'learning_rate': 0.030042259864304366, 'min_child_weight': 19, 'max_depth': 12, 'max_delta_step': 8, 'subsample': 0.21684497186514273, 'colsample_bytree': 0.7003826597151147, 'gamma': 0.02156806207076522, 'n_estimators': 147, 'eta': 0.15337337361538475}
+gradientBoostModel = xgb.XGBClassifier(**gradientBoostParams)
 
 #Set model to use
 model = gradientBoostModel
 
-#Run model and return accuracy
+#Train model
 model.fit(X_train, y_train)
+
+#Get model accuracy
 y_pred = model.predict(X_test)
 accuracy = metrics.accuracy_score(y_test, y_pred)
 print(f'test_accuracy:{accuracy}')
+
+#Save model
+model.save_model("UFC_MODEL_XGBOOST_V1.json")
