42e5fbb80c0e5ebe70628ba2c314a12d5b8b15a2
Donald Kinya Fisher <kfisher.donald@gmail.com>
2026-08-06 17:35:40 -0400

    Addition of predict feature

--- app_pages/predict.py
+++ app_pages/predict.py
@@ -1,34 +1,32 @@
 import streamlit as st
-from app.utils import api_post, api_get
+from app.utils import load_upcoming_events, load_scheduled_fights
 import requests
 import pandas as pd
 from app.ml.training import DEFAULT_COMPARISON_MODELS
+from app.ml.predictor import predict_fights
 
-#TODO: Generate fight label keys
-fight_labels = {}
-selected_label = st.selectbox("Fight", list(fight_labels.keys()))
+#Select Event to generate predictione for
+events_df = load_upcoming_events()
+scheduled_fights_df = load_scheduled_fights()
+selected_event = st.selectbox("Event", events_df['EVENT'] )
 
 selected_model = st.selectbox("Select Model", options = DEFAULT_COMPARISON_MODELS)
 
-
-if st.button("Run Prediction", type="primary"):
-    with st.spinner("Running baseline model..."):
+if st.button("Predict", type="primary"):
+    with st.spinner("Generating Predictions..."):
         try:
-            prediction = api_post("/predictions/run", params={"fight_id": fight_labels[selected_label]})
-            st.success("Prediction saved")
-            st.json(prediction)
+            selected_fights_df = scheduled_fights_df[scheduled_fights_df['event_name'] == selected_event]
+            predictions = predict_fights(selected_fights_df, selected_model)
+            st.dataframe(predictions, use_container_width=True, hide_index=True)
         except requests.RequestException as exc:
             st.error(f"Prediction failed: {exc}")
 
 st.subheader("Saved Predictions")
-try:
-    predictions = api_get("/predictions")
-except requests.RequestException:
-    predictions = []
+saved_predictions = []
 
-if predictions:
+if saved_predictions:
     pred_rows = []
-    for prediction in predictions:
+    for prediction in saved_predictions:
         fight = prediction.get("fight", {})
         pred_rows.append(
             {
